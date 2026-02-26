fae2cc5bf89f241eb62c8af1e2535522f3fbd9aa
MADDOXJUMPER <mjumper2680@floridapoly.edu>
2026-02-26 17:08:44 +0000

    Lab 6 Help

--- Week 7/Lab 6.c
+++ Week 7/Lab 6.c
@@ -14,4 +14,33 @@ int main(){
             scanf("%d", n);
         }
     }
+
+    \\\\\\\\\\\\\\\\\
+
+    int ch;
+
+    printf("Enter choice:");
+    scanf("%d", &ch);
+
+    if(ch == 1){
+        printf("Enter a number:");
+        scanf("%d", &n);
+        if(ch == 1){
+
+            temp = n;
+        int Sum = 0;
+            while(temp > 0){
+            Sum = Sum + temp;
+
+            temp--;
+        }
+        printf("Sum of 1 to n = %d\n", Sum");
+        }
+    }
+
+    }
+    
+
+    
+    
 }
