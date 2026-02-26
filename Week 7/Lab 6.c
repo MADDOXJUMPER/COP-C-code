855e5ab062b3ffa3c86f0be959cb624d4d610520
MADDOXJUMPER <mjumper2680@floridapoly.edu>
2026-02-26 16:45:31 +0000

    Lab 6 Start

---
+++ Week 7/Lab 6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+     
+    int c = 0;
+    int n = 0;
+
+    while( c != 4){
+        printf("===== Menu =====\n1. Sum from 1 to n\n2. Product from 1 to n\n3.Multiplication Table\n4. Exit");
+        printf("Enter your choice:");
+        scanf("%d", &c);
+
+        if(i == 1){
+            printf("Enter a positive integer:");
+            scanf("%d", n);
+        }
+    }
+}
